be965e6a0442be0202ba4a19026a1c2fe80a684e
Sus 304 <linnea.schogarne@elev.ga.klaragymnasium.se>
2023-10-26 10:50:57 +0200

    General HealthController script

--- Food fight/Assets/Scripts/Health/HealthController.cs
+++ Food fight/Assets/Scripts/Health/HealthController.cs
@@ -4,6 +4,51 @@ using UnityEngine;
 
 public class HealthController : MonoBehaviour
 {
+
+    [SerializeField]
+    private float currentHealth;
+
+    [SerializeField]
+    private float maximumHealth;
+
+    public float remainingHealthPercentage
+    {
+        get
+        {
+            return currentHealth / maximumHealth;
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if (currentHealth != 0)
+        {
+            currentHealth -= damage;
+        }
+
+        if (currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
+
+        return;
+        
+    }
+
+    public void AddHealth(float addAmount)
+    {
+        if (currentHealth != maximumHealth)
+        {
+            currentHealth += addAmount;
+        }
+
+        if (currentHealth > maximumHealth)
+        {
+            currentHealth = maximumHealth;
+        }
+    }
+    
+
     // Start is called before the first frame update
     void Start()
     {
