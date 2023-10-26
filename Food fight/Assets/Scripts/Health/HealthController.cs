3c1f1298cd0f8fc0d5c3190105cd189e9c87ac6b
Sus 304 <linnea.schogarne@elev.ga.klaragymnasium.se>
2023-10-26 10:53:20 +0200

    Enemy attacking script creaded

--- Food fight/Assets/Scripts/Health/HealthController.cs
+++ Food fight/Assets/Scripts/Health/HealthController.cs
@@ -49,15 +49,4 @@ public class HealthController : MonoBehaviour
     }
     
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
