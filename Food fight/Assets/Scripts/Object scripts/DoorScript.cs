e0e223d9f15be2eefc2fff153a59eff409b1628f
SkolkontoKlara420 <kevin.backstrom@elev.ga.klaragymnasium.se>
2023-12-01 14:56:06 +0100

    Dörren fungerar nästan perfekt?

---
+++ Food fight/Assets/Scripts/Object scripts/DoorScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    public Transform playerTransform; 
+    public GameObject player; 
+    public Transform doorTransform;
+    public GameObject door;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(player == null)
+        {
+            player = GameObject.FindWithTag("Player");
+        }
+
+        playerTransform = player.transform; 
+        doorTransform = door.transform; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerTransform.position.x < doorTransform.position.x)
+        {
+            GetComponent<BoxCollider2D>().enabled = false;
+        }
+        else
+        {
+            GetComponent<BoxCollider2D>().enabled = true;
+        }
+    }
+}
