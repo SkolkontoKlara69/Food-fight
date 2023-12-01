b9cab25ae7613f0408b082f5d750fceb5523054d
Sus 304 <linnea.schogarne@elev.ga.klaragymnasium.se>
2023-12-01 14:18:53 +0100

    Code for menuebuttons
    The buttons and so on will appear soon

--- Food fight/Assets/Scripts/UI+Menus/Buttonmanager.cs
+++ Food fight/Assets/Scripts/UI+Menus/Buttonmanager.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Buttonmanager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public GameObject settingsMenuObj;
+
+    public GameObject confirmExitMenu;
+    public GameObject confirmMainMenu;
+
+    public void Start()
     {
-        
+        settingsMenuObj.SetActive(false);
+        confirmExitMenu.SetActive(false);
+        confirmMainMenu.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    // Menu related buttons
+    public void OnMenuButtonPress()
     {
-        
+        confirmMainMenu.SetActive(true);
+    }
+
+    public void OnMenuYesPress()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void OnConfirmNoPress()
+    {
+        confirmExitMenu.SetActive(false);
+        confirmMainMenu.SetActive(false);
+    }
+
+    //Exit related buttons
+    public void OnExitButtonPress()
+    {
+        confirmExitMenu.SetActive(true);
+    }
+
+    public void OnExitYesPress()
+    {
+        Application.Quit();
+    }
+
+    //The no-option in the confirm menu for exit is the same as the main menu one
+    
+
+    //Settings related buttons
+    public void OnSettingsPress()
+    {
+        settingsMenuObj.SetActive(true);
     }
 }
