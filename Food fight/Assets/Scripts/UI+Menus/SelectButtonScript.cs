592cdf83056f9297414c245704e9cfbb367d64da
Sus 304 <linnea.schogarne@elev.ga.klaragymnasium.se>
2023-12-07 11:13:34 +0100

    Selecting buttons and inputsystem

---
+++ Food fight/Assets/Scripts/UI+Menus/SelectButtonScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectButtonScript : MonoBehaviour
+{
+    public Button firstMenuButton;
+    public Button firstSettingsButton;
+    GameObject settingsobj = GameObject.FindGameObjectWithTag("settings");
+   
+    void Start()
+    {
+        firstMenuButton.Select();
+    }
+
+    public void Update()
+    {
+        bool settingsActive = settingsobj.activeInHierarchy;
+
+        if (settingsActive)
+        {
+            firstMenuButton
+            firstSettingsButton.Select();
+        }
+    }
+
+}
