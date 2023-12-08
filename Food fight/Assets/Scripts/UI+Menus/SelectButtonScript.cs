a1518fc53cb6af3d20ef2d59345a049b8a7628ca
Sus 304 <linnea.schogarne@elev.ga.klaragymnasium.se>
2023-12-08 14:29:47 +0100

    selectbutton script update

--- Food fight/Assets/Scripts/UI+Menus/SelectButtonScript.cs
+++ Food fight/Assets/Scripts/UI+Menus/SelectButtonScript.cs
@@ -2,28 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class SelectButtonScript : MonoBehaviour
 {
-    /*
-    public Button firstMenuButton;
-    public Button firstSettingsButton;
-    GameObject settingsobj = GameObject.FindGameObjectWithTag("settings");
-   
+    public GameObject firstMenuButton;
+    public GameObject firstSettingsButton;
+    public GameObject firstConfirmMenuButton;
+    public GameObject firstConfirmExitButton;
+    public EventSystem eventSystem;
+
+    GameObject pausObj;
+    GameObject settingsobj;
+    GameObject confirmMenuObj;
+    GameObject confirmExitObj;
+
     void Start()
     {
-        firstMenuButton.Select();
+       settingsobj = GameObject.FindGameObjectWithTag("settings");
+       confirmMenuObj = GameObject.FindGameObjectWithTag("confirmMainMenu");
+       confirmExitObj = GameObject.FindGameObjectWithTag("confirmExit");
+       pausObj = GameObject.FindGameObjectWithTag("PauseButton");
+
+        eventSystem.firstSelectedGameObject = firstMenuButton;
     }
 
     public void Update()
     {
         bool settingsActive = settingsobj.activeInHierarchy;
-
-        if (settingsActive)
+        bool confirmMenuActive = confirmMenuObj.activeInHierarchy;
+        bool confirmExitActive = confirmExitObj.activeInHierarchy;
+        bool pauseActive = pausObj.activeInHierarchy;
+        
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            //firstMenuButton
-            firstSettingsButton.Select();
+            if (settingsActive)
+            {
+
+                eventSystem.SetSelectedGameObject(firstSettingsButton, new BaseEventData(eventSystem));
+            }
+            else if (confirmMenuActive)
+            {
+                eventSystem.SetSelectedGameObject(firstConfirmMenuButton, new BaseEventData(eventSystem));
+
+
+            }
+            else if (confirmExitActive)
+            {
+                eventSystem.SetSelectedGameObject(firstConfirmExitButton, new BaseEventData(eventSystem));
+
+            }
+            else if (pauseActive)
+            {
+                eventSystem.SetSelectedGameObject(firstMenuButton, new BaseEventData(eventSystem));
+            }
         }
+        
     }
-    */
+
+        
+    
 }
