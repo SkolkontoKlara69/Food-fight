0c0aff1345bc50d3c5320246386322566539f951
CODINGWIZZFAN <125278345+CodingWizzFan@users.noreply.github.com>
2024-01-25 10:46:53 +0100

    fix finnish

--- Food fight/Assets/Scripts/Checkpoint + Finish/Finish.cs
+++ Food fight/Assets/Scripts/Checkpoint + Finish/Finish.cs
@@ -11,22 +11,27 @@ public class Finish : MonoBehaviour
     public GameObject finnishCanvas;
     public PlayerMovement movementScript;
 
+    public Rigidbody2D rb2d;
+
     [SerializeField]
     private float duration;
+  
+    private float waitingTime = 2;
 
+    private bool compleet = false;
     private void Start()
     {
-        movementScript = GameObject.Find("Player (2)").GetComponent<PlayerMovement>();
+        
+        movementScript = GameObject.Find("Player (2)").GetComponent<PlayerMovement>();        
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {       
         if (collision.CompareTag("Player"))
         {
-            
+            compleet = true;
             animator.SetBool("PlayerHasThouchedFinish", true);
             StartCoroutine(Finsish(duration));
             finnishCanvas.SetActive(true);
-
         }
     }
 
@@ -34,5 +39,15 @@ public class Finish : MonoBehaviour
     {       
         yield return new WaitForSeconds(duration);       
         SceneController.instance.NextLevel();
+    }
+
+    private void Update()
+    {
+        if(compleet == true)
+        {
+            movementScript.enabled = !movementScript.enabled;
+            rb2d.velocity = new Vector2(0, 0);
+            compleet = false;
+        }
     }
 }
