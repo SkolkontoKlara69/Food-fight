b24eee47ec144e8cbd8b90c4d0e10c5ff559083a
SkolkontoKlara420 <kevin.backstrom@elev.ga.klaragymnasium.se>
2024-01-26 14:11:11 +0100

    Fixade dörren igen

--- Food fight/Assets/Scripts/Object scripts/DoorScript.cs
+++ Food fight/Assets/Scripts/Object scripts/DoorScript.cs
@@ -24,7 +24,7 @@ public class DoorScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (playerTransform.position.x < doorTransform.position.x + 1.25)
+        if (playerTransform.position.x < doorTransform.position.x + 1.5)
         {
             GetComponent<BoxCollider2D>().enabled = false;
         }
