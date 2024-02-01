08bf0e87fa09c0297573cb685ecdaaf532a6d571
SkolkontoKlara420 <kevin.backstrom@elev.ga.klaragymnasium.se>
2024-02-01 10:52:42 +0100

    Fixade enemyHealthBars och updaterade prefabs
    Fixade iaf i London-banan, hoppas det funkar i resten också

--- Food fight/Assets/EnemyHealthBar.cs
+++ Food fight/Assets/EnemyHealthBar.cs
@@ -9,20 +9,44 @@ public class EnemyHealthBar : MonoBehaviour
     [SerializeField] private float reduceSpeed = 1;
     public float target;
 
-    public float currentHealth;
+    public float currentHealth; 
     public float maxHealth;
 
-    /*public void UpdateHealthBar(EnemyHealth enemyHealth)
+    private EnemyHealth associatedEnemyHealth;
+
+    void Start()
     {
-        
-    }*/
+        // Find the parent (enemy) of the health bar
+        Transform enemyTransform = transform.parent;
+
+        if (enemyTransform != null)
+        {
+            // Get the EnemyHealth component from the enemy
+            associatedEnemyHealth = enemyTransform.GetComponentInChildren<EnemyHealth>();
+
+            if (associatedEnemyHealth == null)
+            {
+                Debug.LogError("No EnemyHealth component found in children of the enemy.");
+            }
+        }
+        else
+        {
+            Debug.LogError("EnemyHealthBar must be a child of an enemy object.");
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-        currentHealth = GameObject.FindGameObjectWithTag("enemy").GetComponent<EnemyHealth>().currentHealth;
+        if (associatedEnemyHealth != null)
+        {
+            float target = associatedEnemyHealth.currentHealth / associatedEnemyHealth.maxHealth;
+            healthBarSprite.fillAmount = Mathf.MoveTowards(healthBarSprite.fillAmount, target, reduceSpeed * Time.deltaTime);
+        }
+
+        /*currentHealth = GameObject.FindGameObjectWithTag("enemy").GetComponent<EnemyHealth>().currentHealth;
         maxHealth = GameObject.FindGameObjectWithTag("enemy").GetComponent<EnemyHealth>().maxHealth;
         target = currentHealth / maxHealth; 
-        healthBarSprite.fillAmount = Mathf.MoveTowards(healthBarSprite.fillAmount, target, reduceSpeed * Time.deltaTime); 
+        healthBarSprite.fillAmount = Mathf.MoveTowards(healthBarSprite.fillAmount, target, reduceSpeed * Time.deltaTime); */
     }
 }
