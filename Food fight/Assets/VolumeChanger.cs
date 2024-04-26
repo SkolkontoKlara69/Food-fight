fe6f6b2a5b6bc05d152986a0f70d8a350d958f56
PredatorPrimus <simon@sennero.se>
2024-04-26 13:46:33 +0200

    Master volume slider

--- Food fight/Assets/VolumeChanger.cs
+++ Food fight/Assets/VolumeChanger.cs
@@ -1,20 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
         
 public class VolumeChanger : MonoBehaviour
 {
     private Slider slider;
+    private float test;
+    public AudioMixer mixer;
     // Start is called before the first frame update
     void Start()
     {
-        slider = this.GetComponent<Slider>();
+        slider = GameObject.Find("Audio slider").GetComponent<Slider>();
     }
 
     // Update is called once per frame
     void Update()
-    {
+    { 
+        mixer.SetFloat("MasterVolume", slider.value);
         
     }
+
 }
+
