706f08117b8def9d19053da63a6871f2f2e1ff1c
PredatorPrimus <simon@sennero.se>
2024-05-24 13:44:55 +0200

    inventory i japan och london

--- Food fight/Assets/HatOnPlayer.cs
+++ Food fight/Assets/HatOnPlayer.cs
@@ -3,18 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HatOnPlayer : MonoBehaviour
-{/*
+{
     GameObject canvas;
     InventoryScript inventoryScript;
     // Start is called before the first frame 
     void Start()
-    { canvas = GameObject.FindGameObjectWithTag < "InventoryCanvas" > ();
-        inventoryScript = GameObject.FindGameObjectWithTag<"InventoryCanvas">
+    {
+        inventoryScript = GameObject.Find("InventoryCanvas").GetComponent<InventoryScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }*/
+    }
 }
